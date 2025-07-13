85be309688be4eecdaa3960e775f38d6bb2d4286
SilentNova <68940892+Urbanstormm@users.noreply.github.com>
2025-07-13 19:49:29 -0400

    Murim Cultivation.lua

--- 4737765103.lua
+++ 4737765103.lua
@@ -31,5 +31,6 @@ function Invdiscord(inviteCode)
 end
 Invdiscord("hAsH4bQ6YG")
 LocalPlayer:Kick("❌Script not released yet\njoin the discord for updates\nhttps://discord.gg/hAsH4bQ6YG")
+setclipboard("https://discord.gg/hAsH4bQ6Y")
 
 warn("-SN")
