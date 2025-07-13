8efd73eaa65a7f5f6a7f678e3c65d0e2738ece9b
SilentNova <68940892+Urbanstormm@users.noreply.github.com>
2025-07-13 19:46:52 -0400

    Murim Cultivation.lua

--- 4737765103.lua
+++ 4737765103.lua
@@ -1,48 +1,32 @@
---test
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local keyCheck = LocalPlayer:FindFirstChild("Project Stark Key Check")
-
-local HttpService = game:GetService("HttpService")
-local JSONEncode = HttpService.JSONEncode
-local GenerateGUID = HttpService.GenerateGUID
-local Request = syn and syn.request or request
-
-function discord(inviteCode)
-pcall(function()
-    Request({
-        Url = "http://127.0.0.1:6463/rpc?v=1",
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json",
-            ["Origin"] = "https://discord.com"
-        },
-        Body = JSONEncode(HttpService, {
-            cmd = "INVITE_BROWSER",
-            args = {
-                code = inviteCode
-            },
-            nonce = GenerateGUID(HttpService, false)
-        }),
-    })
-end)
+function Invdiscord(inviteCode)
+    pcall(
+        function()
+            local HttpService = game:GetService("HttpService")
+            local JSONEncode = HttpService.JSONEncode
+            local GenerateGUID = HttpService.GenerateGUID
+            local Request = syn and syn.request or request
+            Request(
+                {
+                    Url = "http://127.0.0.1:6463/rpc?v=1",
+                    Method = "POST",
+                    Headers = {
+                        ["Content-Type"] = "application/json",
+                        ["Origin"] = "https://discord.com"
+                    },
+                    Body = JSONEncode(
+                        HttpService,
+                        {
+                            cmd = "INVITE_BROWSER",
+                            args = {
+                                code = inviteCode
+                            },
+                            nonce = GenerateGUID(HttpService, false)
+                        }
+                    )
+                }
+            )
+        end
+    )
 end
-
-
-if not keyCheck then
-    pcall(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Urbanstormm/Project-Stark/main/Main.lua'))()
-    end)
-    discord("hAsH4bQ6YG")
-
-elseif keyCheck.Value ~= true then
-    pcall(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Urbanstormm/Project-Stark/main/Main.lua'))()
-    end)
-    discord("hAsH4bQ6YG")
-else
-    keyCheck:Destroy()
-    warn("-SN")
-    --script here
-end
+Invdiscord("hAsH4bQ6YG")
+LocalPlayer:Kick("❌Script not released yet\njoin the discord for updates\nhttps://discord.gg/hAsH4bQ6YG")
