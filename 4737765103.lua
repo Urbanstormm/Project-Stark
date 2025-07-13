99ee512cf25fb497635b281e18fbe99e6bc988e1
SilentNova <68940892+Urbanstormm@users.noreply.github.com>
2025-07-13 19:56:11 -0400

    Murim Cultivation.lua

--- 4737765103.lua
+++ 4737765103.lua
@@ -1,35 +1,25 @@
-local LocalPlayer = Players.LocalPlayer
-function Invdiscord(inviteCode)
+function Invdiscord(a)
     pcall(
         function()
-            local HttpService = game:GetService("HttpService")
-            local JSONEncode = HttpService.JSONEncode
-            local GenerateGUID = HttpService.GenerateGUID
-            local Request = syn and syn.request or request
-            Request(
+            local b = game:GetService("HttpService")
+            local c = syn and syn.request or (http and http.request or request)
+            if not c then
+                warn("❌ HTTP request function not available.")
+                return
+            end
+            local d = {cmd = "INVITE_BROWSER", args = {code = a}, nonce = b:GenerateGUID(false)}
+            c(
                 {
                     Url = "http://127.0.0.1:6463/rpc?v=1",
                     Method = "POST",
-                    Headers = {
-                        ["Content-Type"] = "application/json",
-                        ["Origin"] = "https://discord.com"
-                    },
-                    Body = JSONEncode(
-                        HttpService,
-                        {
-                            cmd = "INVITE_BROWSER",
-                            args = {
-                                code = inviteCode
-                            },
-                            nonce = GenerateGUID(HttpService, false)
-                        }
-                    )
+                    Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"},
+                    Body = b:JSONEncode(d)
                 }
             )
         end
     )
 end
 Invdiscord("hAsH4bQ6YG")
-LocalPlayer:Kick("❌Script not released yet\njoin the discord for updates\nhttps://discord.gg/hAsH4bQ6YG")
-setclipboard("https://discord.gg/hAsH4bQ6Y")
-warn("-SN")
+game:GetService("Players").LocalPlayer:Kick(
+    "❌ Script not released yet.\nJoin the Discord for updates:\nhttps://discord.gg/hAsH4bQ6YG"
+)
