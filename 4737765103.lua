c3e1a13c36b21f87b5e9217acadff86d9a4e1bf7
Urbanstorm <68940892+Urbanstormm@users.noreply.github.com>
2025-07-13 15:51:41 -0400

    Update 4737765103.lua

--- 4737765103.lua
+++ 4737765103.lua
@@ -3,14 +3,45 @@ local LocalPlayer = Players.LocalPlayer
 
 local keyCheck = LocalPlayer:FindFirstChild("Project Stark Key Check")
 
-if not keyCheck then
-    LocalPlayer:Kick("🚫 You must pass the Project Stark key system first!")
-elseif keyCheck.Value ~= true then
-    LocalPlayer:Kick("🚫 Your Project Stark key validation failed or expired!")
-else
-    -- Passed the check
-    print("✅ Script Executed Successfully!")
+local HttpService = game:GetService("HttpService")
+local JSONEncode = HttpService.JSONEncode
+local GenerateGUID = HttpService.GenerateGUID
+local Request = syn and syn.request or request
 
-    -- Clean up for neatness
+function discord(inviteCode)
+pcall(function()
+    Request({
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = JSONEncode(HttpService, {
+            cmd = "INVITE_BROWSER",
+            args = {
+                code = inviteCode
+            },
+            nonce = GenerateGUID(HttpService, false)
+        }),
+    })
+end)
+end
+
+
+if not keyCheck then
+    pcall(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Urbanstormm/Project-Stark/main/Main.lua'))()
+    end)
+    discord("hAsH4bQ6YG")
+
+elseif keyCheck.Value ~= true then
+    pcall(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Urbanstormm/Project-Stark/main/Main.lua'))()
+    end)
+    discord("hAsH4bQ6YG")
+else
     keyCheck:Destroy()
+    warn("-SN")
+    --script here
 end
